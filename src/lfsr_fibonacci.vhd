19aa2da6a93cdff7b57e62c7c82bcfc85de2fc46
Jonathan Ott <ott-jonathan@gmx.de>
2021-05-04 13:09:12 +0200

    fixed lfsr

--- src/lfsr_fibonacci.vhd
+++ src/lfsr_fibonacci.vhd
@@ -2,7 +2,7 @@ LIBRARY IEEE;
 USE IEEE.std_logic_1164.ALL;
 USE IEEE.numeric_std.ALL;
 
-entity lsfr_fibonacci is 
+entity lfsr_fibonacci is 
     port(
         clk_i       : in std_ulogic;
         en_pi       : in std_ulogic;
@@ -13,19 +13,18 @@ entity lsfr_fibonacci is
         );
 end entity; 
 
-architecture rtl of lsfr_fibonacci is 
+architecture rtl of lfsr_fibonacci is 
     signal c_state_register, n_state_register : std_ulogic_vector(3 downto 0);
 begin 
 
     --shift
-    n_state_register <= c_state_register(2 downto 0) & 
-                        c_state_register(3) xor c_state_register(2);
+    n_state_register <=  c_state_register(1) xor c_state_register(0) & c_state_register(3 downto 1);
 
     --change output
     c_state_register <= "0001" when rst_n = '0' else n_state_register 
                                 when rising_edge(clk_i) and en_pi = '1';
 
     lfsr_o <= c_state_register;
-    noise_o <= c_state_register(3);
+    noise_o <= c_state_register(0);
     eoc_po <= '1' when c_state_register = "0011"
 end architecture;
