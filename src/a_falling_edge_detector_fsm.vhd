57cdb1ddd9236eca4fbf4212c416acceca1a8681
Ruairí <ruairi.dillon@hs-augsburg.de>
2021-05-11 13:04:32 +0200

    falling edge detector completed

--- src/a_falling_edge_detector_fsm.vhd
+++ src/a_falling_edge_detector_fsm.vhd
@@ -2,11 +2,55 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 architecture fsm of falling_edge_detector is
-  signal q0,q1 : std_ulogic;
+
+type state_t is (high_s, detection_s, low_s, init);
+  signal current_state, next_state: state_t;
+
 begin
 
-  q0 <= '1' when rst_ni = '0' else x_i when rising_edge(clk_i);
-  q1 <= '1' when rst_ni = '0' else q0 when rising_edge(clk_i);
-  fall_o <= '1' when q1 = '1' and q0 = '0' else '0';
+  sm_p : process(x_i, current_state)
+  begin
+    --default assignments
+    fall_o <= '0';
+    next_state <= current_state;
+
+    --output-logic and next-state-logic
+    case current_state is 
+      when init     =>  -- initialising 
+        if x_i = '0' then 
+          next_state <= low_s;
+        else 
+          next_state <= high_s;
+        end if;
+
+      when low_s     =>  -- low state
+
+        if x_i = '0' then 
+          next_state <= low_s;
+        else 
+          next_state <= high_s;
+        end if;
+
+      when high_s     =>   -- high state
+        if x_i = '0' then 
+          next_state <= detection_s;
+        else 
+          next_state <= high_s;
+        end if;
+
+      when detection_s     =>  -- detection state
+	fall_o <= '1';
+        if x_i = '0' then 
+          next_state <= low_s;
+        else 
+          next_state <= high_s;
+
+        end if;
+
+
+end case;
+end process;
+
+    current_state <= init when rst_ni = '0' else next_state when rising_edge(clk_i);
 
 end architecture;
